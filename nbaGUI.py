d493350c9e8eae25ff6577b52bdcb52f62f3f55a
Patrick Xie <pxie2001@gmail.com>
2020-01-15 21:45:43 -0500

    Refactored code

--- nbaGUI.py
+++ nbaGUI.py
@@ -1,35 +1,16 @@
 import pandas as pd
 from tkinter import *
-header = {
-    'functions file'
-}
 
 pdata = pd.read_csv("nbaplayerstats2019.csv")
 
 root = Tk()
 root.title("NBA Filter")
 
-Display = Text(root, height=30,width=150)
-TitleLabel = Label(root,text="Player Data")
-Search = Text(root,height=1, width=43)
-Search.insert(END,"Enter a player's full name (case sensitive)")
-
-leftFrame = Frame(root)
-leftFrame.pack(side=LEFT)
-Display.insert(END,pdata.head(50).to_string())
-TitleLabel.pack()
-Display.pack()
-Search.pack()
-
 def findPlayer(event):
     Display.delete(1.0,END)
     player_name = str(Search.get("1.0",END)).strip()
     player = pdata[pdata['Player'].str.contains(player_name)==True]
     Display.insert(END,player.to_string())
-
-SearchButton = Button(root,text="Search")
-SearchButton.bind("<Button-1>",findPlayer)
-SearchButton.pack()
 
 def topPlayers(stat):
     display = pdata.sort_values(stat,ascending=False)
@@ -60,6 +41,22 @@ def Threes(event):
     Display.delete(1.0,END)
     Display.insert(END,topPlayers("3P"))
 
+Display = Text(root, height=30,width=150)
+TitleLabel = Label(root,text="Player Data")
+Search = Text(root,height=1, width=43)
+Search.insert(END,"Enter a player's full name (case sensitive)")
+
+leftFrame = Frame(root)
+leftFrame.pack(side=LEFT)
+Display.insert(END,pdata.head(50).to_string())
+TitleLabel.pack()
+Display.pack()
+Search.pack()
+
+SearchButton = Button(root,text="Search")
+SearchButton.bind("<Button-1>",findPlayer)
+SearchButton.pack()
+
 PtsButton = Button(leftFrame,text="PTS")
 PtsButton.bind("<Button-1>",PTS)
 PtsButton.pack()
@@ -84,10 +81,4 @@ ThreesButton = Button(leftFrame,text="3PM")
 ThreesButton.bind("<Button-1>",Threes)
 ThreesButton.pack()
 
-# player search touched up, multiple years (2018), 
-#error handling in search
-
 root.mainloop()
-
-    
-
